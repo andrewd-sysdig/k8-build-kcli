21f0a73bb6ba015f6033b7bbce9bfb74bab432dd
Andrew D <andrew.d@sysdig.com>
2023-08-12 08:02:39 +1000

    Fix containerd config.toml

--- k8s-build-scripts/02-install_containerd.sh
+++ k8s-build-scripts/02-install_containerd.sh
@@ -17,25 +17,17 @@ net.ipv4.ip_forward                 = 1
 EOF
 sysctl --system >/dev/null 2>&1
 
-## Install containerd
-#apt-get install containerd -y
-
 # Install containerd.io 
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt update
 sudo apt install -y containerd.io
 
-# https://www.itzgeek.com/how-tos/linux/ubuntu-how-tos/install-containerd-on-ubuntu-22-04.html#Containerd_configuration_for_Kubernetes-2
-cat <<EOF | sudo tee -a /etc/containerd/config.toml
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc]
-[plugins."io.containerd.grpc.v1.cri".containerd.runtimes.runc.options]
-SystemdCgroup = true
-EOF
+# Setup Containerd config.toml and set SystemdCgroup = true
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
 
-sed -i 's/^disabled_plugins \=/\#disabled_plugins \=/g' /etc/containerd/config.toml
-
-# Setup so crictl works
+# Configure container sockets for critctl
 cat <<EOF | sudo tee -a /etc/crictl.yaml
 runtime-endpoint: unix:///run/containerd/containerd.sock
 image-endpoint: unix:///run/containerd/containerd.sock
